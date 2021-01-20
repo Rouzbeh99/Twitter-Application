11a7bd4fab8b249273b00064d0636d3884cdb647
Rouzbeh Paktinat <rouzbehpaktinat99@gmail.com>
2021-01-20 22:47:06 +0330

    Create project

---
+++ tweeter/src/test/java/ir/ac/sbu/tweeter/TweeterApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.ac.sbu.tweeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
